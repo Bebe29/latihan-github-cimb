7fc865910c0b136451c37e07bc8df203932b3bf9
Bebe29 <bevinda.je@gmail.com>
2020-03-12 20:52:25 +0700

    first commit

---
+++ Day 12/index.js
@@ -0,0 +1,22 @@
+let a = {
+    tahun: '1234',
+    nama: 'asdasd',
+    kata: 'kontak'
+}
+
+let b = {tahun: '1234', nama: 'asdasd', bulan: 'Jan'}
+
+let c = {
+    nama: 'asd',
+    kotak: {
+        email: 'email',
+        hp: '0000'
+    }
+}
+
+let d = {...c[a.kata]}
+
+let nama = 'kontak'
+
+c[nama] //variable
+c.nama //property
